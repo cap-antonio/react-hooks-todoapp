1b88c584b2f6f5972df6567d3ea4689d1d5d7553
cap.Antonio <cheremnykh.anton@gmail.com>
2020-02-29 15:20:13 +0100

    Updates

---
+++ precache-manifest.e6613e9cde5d42a3bcf5b8843da0dc52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91091aa65504981919c28fa5fcf866d9",
+    "url": "/react-hooks-todoapp/index.html"
+  },
+  {
+    "revision": "c4c288c4613b98c3084e",
+    "url": "/react-hooks-todoapp/static/css/main.a8f52c40.chunk.css"
+  },
+  {
+    "revision": "d576ac640c9556ececa4",
+    "url": "/react-hooks-todoapp/static/js/2.1b28494c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-hooks-todoapp/static/js/2.1b28494c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4c288c4613b98c3084e",
+    "url": "/react-hooks-todoapp/static/js/main.2681e6db.chunk.js"
+  },
+  {
+    "revision": "d3b6d0371ac959bc8587",
+    "url": "/react-hooks-todoapp/static/js/runtime-main.c491522d.js"
+  }
+]);
