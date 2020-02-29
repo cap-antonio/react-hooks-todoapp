4dd46d1eb1b0fa7fe4196b05ca157ed786857972
cap.Antonio <cheremnykh.anton@gmail.com>
2020-02-29 14:25:03 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-hooks-todoapp/precache-manifest.e6613e9cde5d42a3bcf5b8843da0dc52.js"
+  "/cap-antonio/react-hooks-todoapp.git/precache-manifest.16dca70055b33153d48547eaef965aa1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-hooks-todoapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cap-antonio/react-hooks-todoapp.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
